c832a8079023578b1d0f6c0bf4591a0b5191c3a9
irriss-nn <weitong@ualberta.ca>
2023-01-14 13:09:39 -0700

    small start of landing page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,4 +14,57 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+const Body = ()=>{
+  return(
+    <div id="body">
+      <header/>
+      <card
+        className='section'
+        title='about sth'
+        description='vghvghvhvhbhjhghjbvhghvhg'
+      />
+      <contactContainer/>
+    </div>
+  )
+}
+
+const Header = ()=>{
+  return(
+    <div className='header'>
+      <span className='header-title'>
+        Trip Start Here
+      </span>
+      <br/>
+      <span className="header-text">
+         Sth here not sure gonna be here yet
+      </span>
+
+    </div>
+  )
+}
+
+const Card = (props) =>{
+  return(
+    <div className={props.className} >
+      <div className="small-div">
+        <i classname={props.className}></i>
+      </div>
+
+      <div className="big-div">
+        <span className="div-title">
+          {props.title}
+        </span>
+        <br/>
+        <span>
+          {props.description}
+        </span>
+      </div>
+
+    </div>
+  )
+}
+ReactDOM.render(<p>Hello</p>, document.getElementById('root'));
+
 reportWebVitals();
